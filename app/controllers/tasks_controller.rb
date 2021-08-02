f1842414e3bdb0dbff61a81b7b2904f7ab841259
Guilherme bertho luiz <guilhermebluiz@hotmail.com>
2021-08-02 14:36:11 -0300

    User can edit task, assures the user editing the task is the same that created

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
+  before_action :your_task, only: :edit
 
   def index
     @tasks = Task.all
@@ -10,7 +11,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.create(list_params)
+    @task = Task.create(task_params)
     @task.user = current_user
     if @task.save
       redirect_to root_path
@@ -20,9 +21,34 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    your_task
+    if @task.update(task_params)
+      redirect_to root_path
+    else
+      flash[:notice] = "Description can't be blank"
+      render :edit 
+    end
+  end
+
+
   private
 
-  def list_params
+  def your_task
+    @task = Task.find(params[:id])
+    if current_user != @task.user
+      redirect_to root_path
+      flash[:alert] = "That task is not yours"
+    end
+  end
+
+
+  def task_params
     params.require(:task).permit(:description, :complete)
   end
   
